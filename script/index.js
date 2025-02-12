6e628a92465e317f2b64e7dc849a39fac651b77f
kye1229 <yaeeun0607@gmail.com>
2025-02-12 17:36:14 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -32,7 +32,7 @@ const newSlide = new Swiper('.new_wrap .new_swiper',{
         }
     },
 });
-const dealNum = document.querySelector('.hot_deal_wrap .deal_page');
+const dealNum = document.querySelector('.hot_deal_wrap .now_page');
 const dealNow = document.querySelectorAll('.hot_deal_wrap .swiper-slide');
 const dealTotalNum = document.querySelector('.hot_deal_wrap .total_page');
 dealTotalNum.innerText = dealNow.length;
@@ -45,6 +45,63 @@ const dealSlide = new Swiper('.hot_deal_wrap .deal_swiper',{
     on:{
         slideChange:function(){
             dealNum.textContent = this.realIndex+1;
+        }
+    },
+});
+
+const bestCategory = document.querySelectorAll('.best_categories a');
+function bestRemove(){
+    bestCategory[0].classList.remove('active');
+    bestCategory[1].classList.remove('active');
+    bestCategory[2].classList.remove('active');
+    bestCategory[3].classList.remove('active');
+    bestCategory[4].classList.remove('active');
+    bestCategory[5].classList.remove('active');
+}
+bestCategory[0].addEventListener('click', (e)=>{
+    e.preventDefault();
+    bestRemove();
+    bestCategory[0].classList.add('active');
+});
+bestCategory[1].addEventListener('click',(e)=>{
+    e.preventDefault();
+    bestRemove();
+    bestCategory[1].classList.add('active');
+})
+bestCategory[2].addEventListener('click',(e)=>{
+    e.preventDefault();
+    bestRemove();
+    bestCategory[2].classList.add('active');
+})
+bestCategory[3].addEventListener('click',(e)=>{
+    e.preventDefault();
+    bestRemove();
+    bestCategory[3].classList.add('active');
+})
+bestCategory[4].addEventListener('click',(e)=>{
+    e.preventDefault();
+    bestRemove();
+    bestCategory[4].classList.add('active');
+})
+bestCategory[5].addEventListener('click',(e)=>{
+    e.preventDefault();
+    bestRemove();
+    bestCategory[5].classList.add('active');
+})
+
+const bestNum = document.querySelector('.best_wrap .now_page');
+const bestNow = document.querySelectorAll('.best_wrap .swiper-slide');
+const bestTotalNum = document.querySelector('.best_wrap .total_page');
+bestTotalNum.innerText = bestNow.length;
+const bestSlide = new Swiper('.best_wrap .best_swiper',{
+    speed:1000,
+    navigation:{
+        prevEl:'.best_wrap .prev',
+        nextEl:'.best_wrap .next',
+    },
+    on:{
+        slideChange:function(){
+            bestNum.textContent = this.realIndex+1;
         }
     },
 });
