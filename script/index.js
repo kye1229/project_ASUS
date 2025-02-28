93322c0ac6af5b031a6ea99f1785478c9ac705b4
yaeeun <yaeeun0607@gmail.com>
2025-02-28 16:29:53 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,220 +1,67 @@
-const header = document.querySelector('.header');
-const mainSwiper = new Swiper('#main',{
-    direction:'vertical',
-    mousewheel:true,
-    speed:1000,
-    on:{
-        slideChangeTransitionEnd:function(){
-            setTimeout(()=> ScrollTrigger.refresh(),0)
-        }
-    },
-});
+const notebookTab = document.querySelectorAll('.category_list a');
+function tabRemove(){
+    notebookTab[0].classList.remove('active');
+    notebookTab[1].classList.remove('active');
+    notebookTab[2].classList.remove('active');
+    notebookTab[3].classList.remove('active');
+    notebookTab[4].classList.remove('active');
+    notebookTab[5].classList.remove('active');
+    notebookTab[6].classList.remove('active');
+};
+notebookTab[0].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[0].classList.add('active');
+})
+notebookTab[1].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[1].classList.add('active');
+})
+notebookTab[2].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[2].classList.add('active');
+})
+notebookTab[3].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[3].classList.add('active');
+})
+notebookTab[4].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[4].classList.add('active');
+})
+notebookTab[5].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[5].classList.add('active');
+})
+notebookTab[6].addEventListener('click',(e)=>{
+    e.preventDefault();
+    tabRemove();
+    notebookTab[6].classList.add('active');
+})
 
-const newsSwiper = new Swiper('#main .news_swiper',{
-    speed:1000,
-    slidesPerView:3.2,
-    spaceBetween:200,
-});
-
-gsap.registerPlugin(ScrollTrigger);
-gsap.to('.page2 h2',{
-    scrollTrigger:{
-        trigger:'.page2 h2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.vision1 h3',{
-    scrollTrigger:{
-        trigger:'.vision1 h3',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.vision2 h3',{
-    scrollTrigger:{
-        trigger:'.vision2 h3',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page3 h2',{
-    scrollTrigger:{
-        trigger:'.page3 h2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page3_top p',{
-    scrollTrigger:{
-        trigger:'.page3_top p',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page3 .cdmo1',{
-    scrollTrigger:{
-        trigger:'.page3 .cdmo1',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse', 
-    },
-    opacity:1,
-    top:0,
-});
-gsap.to('.page3 .cdmo2',{
-    scrollTrigger:{
-        trigger:'.page3 .cdmo2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse', 
-    },
-    opacity:1,
-    top:0,
-});
-gsap.to('.page3 .cdmo3',{
-    scrollTrigger:{
-        trigger:'.page3 .cdmo3',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse', 
-    },
-    opacity:1,
-    top:0,
-});
-
-gsap.to('.page4 h2',{
-    scrollTrigger:{
-        trigger:'.page4 h2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page4 .quality_list p',{
-    scrollTrigger:{
-        trigger:'.page4 .quality_list p',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    top:0,
-});
-
-gsap.to('.page5 h2',{
-    scrollTrigger:{
-        trigger:'.page5 h2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page5 p',{
-    scrollTrigger:{
-        trigger:'.page5 p',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page5 .news_swiper',{
-    scrollTrigger:{
-        trigger:'.page5 .news_swiper',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    top:0,
-});
-gsap.to('.page6 h2',{
-    scrollTrigger:{
-        trigger:'.page6 h2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-gsap.to('.page6_top p',{
-    scrollTrigger:{
-        trigger:'.page6_top p',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    left:0,
-});
-
-gsap.to('.page6 .talent1',{
-    scrollTrigger:{
-        trigger:'.page6 .talent1',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    top:0,
-});
-gsap.to('.page6 .talent2',{
-    scrollTrigger:{
-        trigger:'.page6 .talent2',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    top:0,
-});
-gsap.to('.page6 .talent3',{
-    scrollTrigger:{
-        trigger:'.page6 .talent3',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    top:0,
-});
-gsap.to('.page6 .talent4',{
-    scrollTrigger:{
-        trigger:'.page6 .talent4',
-        start:'top 80%',
-        end:'top 20%',
-        toggleActions:'play reverse restart reverse',
-    },
-    opacity:1,
-    top:0,
-});
+const pageNum = document.querySelectorAll('.page_list a');
+function numRemove(){
+    pageNum[0].classList.remove('active');
+    pageNum[1].classList.remove('active');
+    pageNum[2].classList.remove('active');
+}
+pageNum[0].addEventListener('click',(e)=>{
+    e.preventDefault();
+    numRemove();
+    pageNum[0].classList.add('active');
+})
+pageNum[1].addEventListener('click',(e)=>{
+    e.preventDefault();
+    numRemove();
+    pageNum[1].classList.add('active');
+})
+pageNum[2].addEventListener('click',(e)=>{
+    e.preventDefault();
+    numRemove();
+    pageNum[2].classList.add('active');
+})
